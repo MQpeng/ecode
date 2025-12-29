7b493b9ff4939d70d065c5e9e9ec77e8c3eb9c3d
Bean <742486126@QQ.COM>
2025-12-29 08:58:51 +0000

    chore(structure): remove old structure package (prepare for redesign)

--- packages/structure/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './types'
-export * from './utils'
-export * from './parser'
-export * from './page-tree'
-
--- packages/structure/src/utils.ts
+++
@@ -1,9 +0,0 @@
-let __idCounter = 1
-export function generateId(prefix = 'node') {
-  return `${prefix}-${__idCounter++}`
-}
-
-// for tests or debugging
-export function resetIdCounter(n = 1) {
-  __idCounter = n
-}
