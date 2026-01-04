576fc3a3106d40b7dd94c5d654bcb91bb653eab6
jockerspice123-wq <jockerspice123@gmail.com>
2026-01-04 04:04:03 +0000

    fix: correct ConfigLoaderFn type signature to accept arguments

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 /**
  * Config loader function type. A loader returns a value of type T.
  */
-export type ConfigLoaderFn<T> = () => T;
+export type ConfigLoaderFn<T> = (...args: any[]) => T;
 
 /**
  * A registry for config loader functions keyed by string.
